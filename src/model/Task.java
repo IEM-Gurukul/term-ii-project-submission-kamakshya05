0e9a95a166679326edcc3a443a569dd74dd1b561
kamakshya05 <kamakshyaghosh2005@gmail.com>
2026-03-22 12:33:32 +0530

    Created Task class

---
+++ src/model/Task.java
@@ -0,0 +1,14 @@
+// src/model/Task.java
+package model;
+
+public class Task {
+    private final String title;
+
+    public Task(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
