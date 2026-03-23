6c9660252fc4cdfaf93e819c4b3a6571a3fef568
kamakshya05 <kamakshyaghosh2005@gmail.com>
2026-03-24 01:34:15 +0530

    Create service/TaskManager.java

---
+++ src/service/TaskManager.java
@@ -0,0 +1,18 @@
+package service;
+
+import model.Task;
+import java.util.ArrayList;
+
+public class TaskManager {
+    ArrayList<Task> tasks = new ArrayList<>();
+
+    public void addTask(String title) {
+        tasks.add(new Task(title));
+    }
+
+    public void viewTasks() {
+        for (Task t : tasks) {
+            t.display();
+        }
+    }
+}
